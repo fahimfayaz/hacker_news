09bdd459ae6783cb0098e788da72e057c703140f
unknown <noel.alam9999@gmail.com>
2021-05-01 20:25:16 +0600

    increment fixed

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export const App= ()=> {
     return(
     <> <StoriesContainer />  
     
-      {/* <CustomizedButtons/>
-    <Footer/> */}
+      <CustomizedButtons/>
+    <Footer/>
     
     
     </>)}
